3b1fc7648764db0b602e91e83548d7748d26714c
nhuyha <nhuy1396@gmail.com>
2026-08-26 14:24:39 +0700

    fix(invoice): remove invoice from global search

--- packages/core/src/modules/invoice/search.ts
+++
@@ -1,8 +0,0 @@
-import type { SearchModuleConfig } from '@open-mercato/shared/modules/search'
-
-export const searchConfig: SearchModuleConfig = {
-  entities: [],
-}
-
-export default searchConfig
-export const config = searchConfig
